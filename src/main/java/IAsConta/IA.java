9f7f1f159ee292763b264d2b095f3653b47f52fb
mateusscavalheiro-afk <mateus_s_cavalheiro@estudante.sesisenai.org.br>
2025-12-12 08:09:11 -0300

    sla

---
+++ src/main/java/IAsConta/IA.java
@@ -0,0 +1,14 @@
+package IAsConta;
+@SuppressWarnings("all")
+
+public class IA {
+    public int id_api;
+
+    public void interagir_user(){
+        System.out.println("RESPONDENDO USUÁRIO");
+    }
+
+    public void ver_dados(){
+        System.out.println("VERIFICANDO DADOS");
+    }
+}
